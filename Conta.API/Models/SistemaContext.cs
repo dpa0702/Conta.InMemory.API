d25d4f1204e5ff9300929e6a53334fee76a0080c
dpa0702 <56276309+dpa0702@users.noreply.github.com>
2022-12-04 20:44:12 -0300

    carga inicial

---
+++ Conta.API/Models/SistemaContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conta.API.Models
+{
+	public class SistemaContext : DbContext
+	{
+		public SistemaContext(DbContextOptions<SistemaContext> options): base(options)
+		{
+		}
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
+
---
+++ Conta.API/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Conta.API.Models
+{
+	public class Usuario
+	{
+		public Usuario()
+		{
+		}
+
+        public int id { get; set; }
+
+		public string login { get; set; }
+
+		public string nome { get; set; }
+    }
+}
+
